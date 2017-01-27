9b18e34458f9b4b44a402fc6b8cc25586a7e042d
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-01-27 16:57:13 +0530

    Corrected java for ubuntu

--- PuppetResources/OtherExamples/cyclos/manifests/install_java.pp
+++ PuppetResources/OtherExamples/cyclos/manifests/install_java.pp
@@ -1,7 +1,35 @@
 class cyclos::install_java {
-    
-    java::oracle { 'jdk6':
-        ensure  => 'present',
-        version => '6',  
+
+
+    if $facts['os']['name'] == 'Ubuntu' {
+
+        class { 'apt': }
+
+        package { "python-software-properties": }
+
+        apt::ppa { 'ppa:webupd8team/java': }
+
+        exec {
+            'set-licence-selected':
+            command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections';
+
+            'set-licence-seen':
+            command => '/bin/echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections';
+        }
+
+        package { 'oracle-java6-installer':
+            ensure => "installed",
+            require => [Apt::Ppa['ppa:webupd8team/java'], Exec['set-licence-selected'], Exec['set-licence-seen']],
+        }
+
     }
+    else {
+        java::oracle { 'jdk6':
+            ensure  => 'present',
+            version => '6',  
+        }
+
+    }
+    
+    
 }
