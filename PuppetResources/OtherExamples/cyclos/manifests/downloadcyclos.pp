39cb2e9dedf7822a9e749b4ca1726ff90e1bb4f3
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-01-27 13:09:36 +0530

    Added fix for file

--- PuppetResources/OtherExamples/cyclos/manifests/downloadcyclos.pp
+++ PuppetResources/OtherExamples/cyclos/manifests/downloadcyclos.pp
@@ -1,4 +1,4 @@
-class cyclos::downloadcyclos($install_path='/home/vagrant'){
+class cyclos::downloadcyclos($install_path='/home/vagrant/cyclos_3.7.3.zip'){
      $cyclos_path = "https://downloads.sourceforge.net/project/cyclos/Cyclos3/3.7.3/cyclos_3.7.3.zip"
     file { $install_path :
         ensure => present ,
